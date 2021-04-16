b9d971728c4dd6b555741ebcfcadae318eab48c9
hee-c <ways2kim@gmail.com>
2021-04-16 18:54:53 +0900

    Add category model

--- models/Category.js
+++ models/Category.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: true,
+  },
+  videos: [{
+    url: {
+      type: String,
+      trim: true,
+    },
+  }],
 });
 
 module.exports = mongoose.model('Category', categorySchema);
