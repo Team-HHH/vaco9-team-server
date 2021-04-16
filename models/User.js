b345532d0b835bfb4d0e0535ade9b1646d9692f1
hee-c <ways2kim@gmail.com>
2021-04-16 18:45:02 +0900

    Add user model

--- models/User.js
+++ models/User.js
@@ -1,6 +1,17 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: true,
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model('User', userSchema);
