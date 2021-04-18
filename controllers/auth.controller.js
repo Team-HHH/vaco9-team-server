f49d99a7b22528c87488090e24f9c91e6240d9cd
KYOUNGHOYU <qmfahs@naver.com>
2021-04-18 15:52:04 +0900

    Add mockup data for response to login request

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -64,6 +64,12 @@ exports.login = async function (req, res, next) {
           process.env.JWT_SECRET,
           { expiresIn: '5H' },
         ),
+        user: {
+          email: "ykh@naver.com",
+          name: "유경호",
+          companyName: "바닐라코딩",
+          companyEmail: "https://www.vanillacoding.co/",
+        },
       },
     });
   } catch (err) {
