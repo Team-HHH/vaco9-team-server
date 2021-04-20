b1c2bae3ec1683d3dc666c6855ed9a5d49728200
hee-c <ways2kim@gmail.com>
2021-04-20 20:05:07 +0900

    Fix verifyAdvertiser, add middleware to routes

--- routes/campaign.js
+++ routes/campaign.js
@@ -4,12 +4,13 @@ const {
   createCampaignValidation,
   campaignStatsValidation,
 } = require('../middlewares/validateInput');
+const { verifyAdvertiser } = require('../middlewares/verifyAdvertiser');
 
 const router = express.Router();
 
-router.post('/', createCampaignValidation, campaignController.createCampaign);
+router.post('/', verifyAdvertiser, createCampaignValidation, campaignController.createCampaign);
 router.get('/popup', campaignController.getCampaignPopUp);
 router.patch('/stats', campaignStatsValidation, campaignController.updateCampaignStats);
-router.get('/:advertiserId', campaignController.getAdvertiserCampaigns);
+router.get('/:advertiserId', verifyAdvertiser, campaignController.getAdvertiserCampaigns);
 
 module.exports = router;
